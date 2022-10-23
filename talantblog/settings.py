8e719f7a8a80781fd28d2f4a5e7382da39c0c5ff
TalandBotirjon <ergashev_botirjon@mail.ru>
2022-10-23 22:31:36 +0500

    False

--- talantblog/settings.py
+++ talantblog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-40^vlc$%%w7o01%x-dkk#=l#s)20lds-b361(atfo^#7(o2m$m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
