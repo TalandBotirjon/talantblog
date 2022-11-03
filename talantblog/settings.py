4ea1a95fde6a05fda37cd25ad5a0c641d49e54e4
TalandBotirjon <ergashev_botirjon@mail.ru>
2022-11-03 14:35:44 +0500

    new post

--- talantblog/settings.py
+++ talantblog/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os.path
 from pathlib import Path
 
-import whitenoise.storage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
